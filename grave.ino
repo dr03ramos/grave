b6411dbd81ae139485d52742a67bb17224bb3952
ramos <50461210+dr03ramos@users.noreply.github.com>
2023-05-06 15:34:16 -0300

    arrumando algumas coisas

--- grave.ino
+++ grave.ino
@@ -1,11 +1,10 @@
-//Programa: Ponte H L298N com motor de passo
-//Autor: Arduino e Cia
-//Conexoes modulo - Arduino
+#include <EEPROM.h>
+
 int IN1 = 8;
 int IN2 = 9;
 int IN3 = 10;
 int IN4 = 11;
-int I/O = 12;
+int IbarraO = 12;
 
 // Define o tempo de cada passo do motor
 int tempo = 5;
@@ -14,7 +13,7 @@ int tempo = 5;
 unsigned long contador;
 
 // salva dados na EEPROM
-void inicializa_leitura_memoria(){
+void inicializa_leitura_memoria() {
     Serial.println("Fazendo leitura...");
 
     // faz leitura da eeprom
@@ -23,193 +22,153 @@ void inicializa_leitura_memoria(){
     Serial.print("Número do contador: ");
     Serial.println(contador);
 }
-void atualiza_leitura_memoria(){
+
+void atualiza_leitura_memoria() {
     // salvar o valor do contador na posição zero
-    endereco_EEPROM = 0;
+    int endereco_EEPROM = 0;
     int valor_EEPROM1 = contador;
     EEPROM.update(endereco_EEPROM, valor_EEPROM1);
 }
-void gira_descer(){
-  // Faz o motor girar em passos
-  // Passo 1
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo);  
-  // Passo 2
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo);
-  // Passo 3
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
-  // Passo 4
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
-  contador--;
-}
-void gira_descer(){
-  // Faz o motor girar em passos
-    // Passo 4
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
-  // Passo 3
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
-  // Passo 2
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo);
-  // Passo 1
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo);  
-  contador++;
-}
-void setup()
-{
-  // Define os pinos como saída
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(I/O, INPUT);
+
+void gira_motor(int velocidade) {
+    if (velocidade > 0) {
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+    }
+    // Se a velocidade for zero, o motor para
+    else if (velocidade == 0) {
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+    }
+    // Se a velocidade for negativa, o motor gira em outro sentido
+    else if (velocidade < 0) {
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+    }
 }
 
-void loop(){
-  if (digitalRead(I/O, HIGH)){
-    contador = 1000;
-    if (contador == 0) {
-      // Para o motor
-      digitalWrite(IN1, 0);
-      digitalWrite(IN2, 0);
-      digitalWrite(IN3, 0);
-      digitalWrite(IN4, 0);
-    } 
-    else {
-      gira_descer();
-    }
-  }
-  else{
-    contador = 0;
-    if (contador >= 1000) {
-    // Para o motor
-    digitalWrite(IN1, 0);
-    digitalWrite(IN2, 0);
-    digitalWrite(IN3, 0);
-    digitalWrite(IN4, 0);
-    } 
-    else {
-    gira_subir();
-    }
-  }
+void setup() {
+    // Define os pinos como saída
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(IbarraO, INPUT);
 }
 
-/*//Pinos de conexao do modulo TCS230  
-const int s0 = 8;  
-const int s1 = 9;  
-const int s2 = 12;  
-const int s3 = 11;  
-const int out = 10;   
-   
+void loop() {
+
+    if (digitalRead(IbarraO) == HIGH) {
+        contador = 1000;
+        if (contador == 0) {
+            // Para o motor
+            digitalWrite(IN1, 0);
+            digitalWrite(IN2, 0);
+            digitalWrite(IN3, 0);
+            digitalWrite(IN4, 0);
+        } else {
+            gira_descer();
+        }
+    } else {
+        contador = 0;
+        if (contador >= 1000) {
+            // Para o motor
+            digitalWrite(IN1, 0);
+            digitalWrite(IN2, 0);
+            digitalWrite(IN3, 0);
+            digitalWrite(IN4, 0);
+        } else {
+            gira_subir();
+        }
+    }
+}
+/*
+//Pinos de conexao do modulo TCS230  
+const int s0 = 8;
+const int s1 = 9;
+const int s2 = 12;
+const int s3 = 11;
+const int out = 10;
+
 //Pinos dos leds  
-int pinoledverm = 2;  
-int pinoledverd = 3;  
-int pinoledazul = 4;  
-    
+int pinoledverm = 2;
+int pinoledverd = 3;
+int pinoledazul = 4;
+
 //Variaveis que armazenam o valor das cores  
-int red = 0;  
-int green = 0;  
-int blue = 0;  
-    
-void setup()   
-{  
-  pinMode(s0, OUTPUT);  
-  pinMode(s1, OUTPUT);  
-  pinMode(s2, OUTPUT);  
-  pinMode(s3, OUTPUT);  
-  pinMode(out, INPUT);  
-  pinMode(pinoledverm, OUTPUT);  
-  pinMode(pinoledverd, OUTPUT);  
-  pinMode(pinoledazul, OUTPUT);  
-  Serial.begin(9600);  
-  digitalWrite(s0, HIGH);  
-  digitalWrite(s1, LOW);  
-}  
-    
-void loop() 
-{  
-  color(); //Chama a rotina que le as cores  
-  //Mostra no serial monitor os valores detectados  
-  Serial.print("Vermelho :");  
-  Serial.print(red, DEC);  
-  Serial.print(" Verde : ");  
-  Serial.print(green, DEC);  
-  Serial.print(" Azul : ");  
-  Serial.print(blue, DEC);  
-  Serial.println();  
-  //Verifica se a cor vermelha foi detectada  
-  if (red < blue && red < green && red < 100)  
-  {  
-   Serial.println("Vermelho");  
-   digitalWrite(pinoledverm, HIGH); //Acende o led vermelho  
-   digitalWrite(pinoledverd, LOW);  
-   digitalWrite(pinoledazul, LOW);  
-  }  
-  //Verifica se a cor azul foi detectada  
-  else if (blue < red && blue < green)   
-  {  
-   Serial.println("Azul");  
-   digitalWrite(pinoledverm, LOW);  
-   digitalWrite(pinoledverd, LOW);  
-   digitalWrite(pinoledazul, HIGH); //Acende o led azul  
-  }  
-  //Verifica se a cor verde foi detectada  
-  else if (green < red && green < blue)  
-  {  
-   Serial.println("Verde");  
-   digitalWrite(pinoledverm, LOW);  
-   digitalWrite(pinoledverd, HIGH); //Acende o led verde  
-   digitalWrite(pinoledazul, LOW);  
-  }  
-  Serial.println();  
-  //Aguarda 2 segundos, apaga os leds e reinicia o processo  
-  delay(2000);   
-  digitalWrite(pinoledverm, LOW);  
-  digitalWrite(pinoledverd, LOW);  
-  digitalWrite(pinoledazul, LOW);  
- }  
-    
-void color()  
-{  
-  //Rotina que le o valor das cores  
-  digitalWrite(s2, LOW);  
-  digitalWrite(s3, LOW);  
-  //count OUT, pRed, RED  
-  red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
-  digitalWrite(s3, HIGH);  
-  //count OUT, pBLUE, BLUE  
-  blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
-  digitalWrite(s2, HIGH);  
-  //count OUT, pGreen, GREEN  
-  green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
+int red = 0;
+int green = 0;
+int blue = 0;
+
+void setup() {
+    pinMode(s0, OUTPUT);
+    pinMode(s1, OUTPUT);
+    pinMode(s2, OUTPUT);
+    pinMode(s3, OUTPUT);
+    pinMode(out, INPUT);
+    pinMode(pinoledverm, OUTPUT);
+    pinMode(pinoledverd, OUTPUT);
+    pinMode(pinoledazul, OUTPUT);
+    Serial.begin(9600);
+    digitalWrite(s0, HIGH);
+    digitalWrite(s1, LOW);
+}
+
+void loop() {
+    color(); //Chama a rotina que le as cores  
+    //Mostra no serial monitor os valores detectados  
+    Serial.print("Vermelho :");
+    Serial.print(red, DEC);
+    Serial.print(" Verde : ");
+    Serial.print(green, DEC);
+    Serial.print(" Azul : ");
+    Serial.print(blue, DEC);
+    Serial.println();
+    //Verifica se a cor vermelha foi detectada  
+    if (red < blue && red < green && red < 100) {
+        Serial.println("Vermelho");
+        digitalWrite(pinoledverm, HIGH); //Acende o led vermelho  
+        digitalWrite(pinoledverd, LOW);
+        digitalWrite(pinoledazul, LOW);
+    }
+    //Verifica se a cor azul foi detectada  
+    else if (blue < red && blue < green) {
+        Serial.println("Azul");
+        digitalWrite(pinoledverm, LOW);
+        digitalWrite(pinoledverd, LOW);
+        digitalWrite(pinoledazul, HIGH); //Acende o led azul  
+    }
+    //Verifica se a cor verde foi detectada  
+    else if (green < red && green < blue) {
+        Serial.println("Verde");
+        digitalWrite(pinoledverm, LOW);
+        digitalWrite(pinoledverd, HIGH); //Acende o led verde  
+        digitalWrite(pinoledazul, LOW);
+    }
+    Serial.println();
+    //Aguarda 2 segundos, apaga os leds e reinicia o processo  
+    delay(2000);
+    digitalWrite(pinoledverm, LOW);
+    digitalWrite(pinoledverd, LOW);
+    digitalWrite(pinoledazul, LOW);
+}
+
+void color() {
+    //Rotina que le o valor das cores  
+    digitalWrite(s2, LOW);
+    digitalWrite(s3, LOW);
+    //count OUT, pRed, RED  
+    red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+    digitalWrite(s3, HIGH);
+    //count OUT, pBLUE, BLUE  
+    blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+    digitalWrite(s2, HIGH);
+    //count OUT, pGreen, GREEN  
+    green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
 }*/
